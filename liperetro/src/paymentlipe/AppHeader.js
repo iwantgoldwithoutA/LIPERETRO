b7431b2c8497a4139920384457275d6c37030836
Ammar trithakoon <whatislifetellme@gmail.com>
2022-03-25 23:50:58 +0700

    เอิร์นแก้ด้วย

---
+++ liperetro/src/paymentlipe/AppHeader.js
@@ -0,0 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
+
+function AppHeader() { 
+
+
+
+    const locate = useLocation();
+    const {book} = locate.state
+    console.log(book)
+    return(
+        <>
+        <h4 className="app-header">PAYMENT ช่องทางการจ่ายเงิน</h4>
+        <h4 className="price-header"> {book.Prices} Bath</h4>
+        
+        </>
+    );
+}
+export default AppHeader; 
