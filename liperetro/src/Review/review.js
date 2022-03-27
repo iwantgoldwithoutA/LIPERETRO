0db81924af815a9dab70a553ffa7ac7e8c1c4d44
Nutnarin Jowantha <6410110144@psu.ac.th>
2022-03-28 03:42:35 +0700

    review 80%

--- liperetro/src/Review/review.js
+++ liperetro/src/Review/review.js
@@ -7,138 +7,268 @@ import {Grid, Typography,Toolbar, AppBar, Rating, FormControl} from "@mui/materi
 import LRbar from '../bar/Bar';
 
 
-function Review(){
-    const [data ,setData] = useState([])
-    const [comment, setComment] = useState({
+// function Review(){
+//     const [data ,setData] = useState([])
+//     const [comment, setComment] = useState({
 
-        Headers: "",
+//         Headers: "",
     
-        Descriptions: "",
+//         Descriptions: "",
 
-        Rating: null,
+//         Rating: null,
 
-        Name:`${localStorage.getItem('UsernameDP')}`,
+//         Name:`${localStorage.getItem('UsernameDP')}`,
 
         
-        users_permissions_users:localStorage.getItem('userID')
+//         users_permissions_users:localStorage.getItem('userID')
 
 
     
     
-    });
+//     });
 
-    function handleSubmit(e) {
+//     function handleSubmit(e) {
 
-        e.preventDefault();
+//         e.preventDefault();
     
-        let url = "http://localhost:1337/reviews";
+//         let url = "http://localhost:1337/reviews";
     
-        fetch(url, {
+//         fetch(url, {
     
-          method: "POST",
+//           method: "POST",
     
-          headers: { "Content-type": "application/json" },
+//           headers: { "Content-type": "application/json" },
     
-          body: JSON.stringify(comment),
+//           body: JSON.stringify(comment),
     
-        })
+//         })
     
-          .then((response) => {
+//           .then((response) => {
     
-            // fetchHeros();
+//             // fetchHeros();
     
-            setComment({Headers:"", Descriptions:"" ,Rating:null, Name:localStorage.getItem('UsernameDP') ,users_permissions_users:localStorage.getItem('userID') })
+//             setComment({Headers:"", Descriptions:"" ,Rating:null, Name:localStorage.getItem('UsernameDP') ,users_permissions_users:localStorage.getItem('userID') })
     
-          })
+//           })
     
-          .catch((err) => console.log(err));
+//           .catch((err) => console.log(err));
     
-      }
+//       }
 
-      useEffect(async () => {
-        try {
-          const res = await fetch('http://localhost:1337/reviews')
-          const set_data = await res.json()
-          setData(set_data)
-          console.log(set_data)
-        } catch (error) {
-            console.log(error)
-          }
-     }, [])
+//       useEffect(async () => {
+//         try {
+//           const res = await fetch('http://localhost:1337/reviews')
+//           const set_data = await res.json()
+//           setData(set_data)
+//           console.log(set_data)
+//         } catch (error) {
+//             console.log(error)
+//           }
+//      }, [])
+//     return(
+//         <>
+//         <LRbar/>
+//         <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet"/>
+//         <div className="backgrand-review">
+//             <h1 className="Text">Review</h1>
+//             <div>
+//                 {data.map((item)=> {
+//                     return(
+//                         <div>
+//                 <Grid className="Tao" container spacing={2} columns={4}>
+//                     <Grid item xs={1}>
+//                     <Avatar className="avt1" alt=" " src={require("./image/avatar.png")} sx={{ width: 50, height: 50 }}/>
+//                     </Grid>
+//                     <Grid itam xs={3}>
+//                         <Typography fontFamily={'Mitr'} variant="h6" component="div" color="#6AB7D6">
+//                         {item.Name}
+//                         </Typography>
+//                         <Typography fontFamily={'Mitr'} variant="h6" component="div" color="#6AB7D6">
+//                         {item.Headers}
+//                         </Typography>
+//                         <Typography>
+//                             <Rating color="primary" value={item.Rating} name='read-only' readOnly />
+//                             {/* <StarBorderIcon color="primary"/>
+//                             <StarBorderIcon color="primary"/>
+//                             <StarBorderIcon color="primary"/>
+//                             <StarBorderIcon color="primary"/> */}
+//                             </Typography>
+//                         <Typography fontFamily={'Mitr'} variant="body2" color="#6AB7D6">
+//                         {item.Descriptions}
+//                         </Typography>
+//                     </Grid>
+//                 </Grid>
+//                 </div>
+//                     )
+//                 })}
+//             </div>
+//             {/* <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 , backgroundColor: '#6AB7D6' }}> */}
+//             {localStorage.getItem("is_auth") ? (
+//                 <div className="box-review"> 
+//                 {/* <Toolbar>  */}
+                
+//                 {/* <Typography component="legend"></Typography> */}
+//                 {/* <Rating name="customized-10" defaultValue={comment.Rating} max={5} size="large" onChange={(e) => setComent({ ...comment, Rating: e.target.value })} /> */}
+//                 {/* </Toolbar> */}
+//                 <Typography className="Teen">
+//                 <FormControl> 
+//                     <div className="form-star" >
+//                     <form className="form"   onSubmit={handleSubmit}>
+//                     {/* <label for="fname"></label> */}
+//                     <div className='box-ii'> 
+//                     <Avatar className="avt2" alt=" " src={require("./image/avatar.png")} sx={{ width: 40, height: 40 }}/>
+//                     <div className='box-iii'>  
+//                     <Rating name="customized-10"   defaultValue={comment.Rating} max={5} size="large" onChange={(e) => setComment({ ...comment, Rating: e.target.value })} />
+//                     </div> 
+//                     </div>
+                    
+                                                              
+//                     <input type="text1" id="fname" name="fname" className="fname" placeholder="Headers" value={comment.Headers} onChange={(e) => setComment({ ...comment, Headers: e.target.value })} />
+                    
+//                     <input type="text2" id="fname1" name="fname1" className="fname1" placeholder="Comment" value={comment.Descriptions} onChange={(e) => setComment({ ...comment, Descriptions: e.target.value })} />
+                    
+//                     <input type="submit" value="Send" ></input>
+//                     </form>
+//                     </div>
+//                 </FormControl> 
+//                 </Typography> 
+//             </div>   
+//             ):( <></> )}
+            
+//             {/* </AppBar> */}
+//         </div>
+//         </>
+//     )
+// }
+
+// export default Review;
+
+function Review(){
     return(
         <>
         <LRbar/>
-        <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet"/>
-        <div className="backgrand-review">
+        <div>
             <h1 className="Text">Review</h1>
             <div>
-                {data.map((item)=> {
-                    return(
-                        <div>
+                <div>
                 <Grid className="Tao" container spacing={2} columns={4}>
                     <Grid item xs={1}>
-                    <Avatar className="avt1" alt=" " src={require("./image/avatar.png")} sx={{ width: 50, height: 50 }}/>
+                    <Avatar className="Tree" alt=" " src={require("./image/avatar.png")} />
                     </Grid>
                     <Grid itam xs={3}>
-                        <Typography fontFamily={'Mitr'} variant="h6" component="div" color="#6AB7D6">
-                        {item.Name}
-                        </Typography>
-                        <Typography fontFamily={'Mitr'} variant="h6" component="div" color="#6AB7D6">
-                        {item.Headers}
+                        <Typography variant="h6" component="div" color="#6AB7D6">
+                        MAXIM CONNER
                         </Typography>
                         <Typography>
-                            <Rating color="primary" value={item.Rating} name='read-only' readOnly />
-                            {/* <StarBorderIcon color="primary"/>
+                            <StarIcon color="primary"/>
                             <StarBorderIcon color="primary"/>
                             <StarBorderIcon color="primary"/>
-                            <StarBorderIcon color="primary"/> */}
+                            <StarBorderIcon color="primary"/>
+                            <StarBorderIcon color="primary"/>
                             </Typography>
-                        <Typography fontFamily={'Mitr'} variant="body2" color="#6AB7D6">
-                        {item.Descriptions}
+                        <Typography variant="body2" color="#6AB7D6">
+                        It's not free
                         </Typography>
                     </Grid>
                 </Grid>
                 </div>
-                    )
-                })}
-            </div>
-            {/* <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 , backgroundColor: '#6AB7D6' }}> */}
-            {localStorage.getItem("is_auth") ? (
-                <div className="box-review"> 
-                {/* <Toolbar>  */}
                 
-                {/* <Typography component="legend"></Typography> */}
-                {/* <Rating name="customized-10" defaultValue={comment.Rating} max={5} size="large" onChange={(e) => setComent({ ...comment, Rating: e.target.value })} /> */}
-                {/* </Toolbar> */}
-                <Typography className="Teen">
+                <div>
+                <Grid className="Tao" container spacing={2} columns={4}>
+                    <Grid item xs={1}>
+                    <Avatar className="Tree" alt=" " src={require("./image/avatar.png")} />
+                    </Grid>
+                    <Grid itam xs={3}>
+                        <Typography variant="h6" component="div" color="#6AB7D6">
+                        DOLA BIN DOLAH
+                        </Typography>
+                        <Typography>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarBorderIcon color="primary"/>
+                        <StarBorderIcon color="primary"/>
+                        <StarBorderIcon color="primary"/>
+                            </Typography>
+                        <Typography variant="body2" color="#6AB7D6">
+                        I use Nokia 3310
+                        </Typography>
+                    </Grid>
+                </Grid>
+                </div>
+                
+                <div>
+                <Grid className="Tao" container spacing={2} columns={4}>
+                    <Grid item xs={1}>
+                    <Avatar className="Tree" alt=" " src={require("./image/avatar.png")}/>
+                    </Grid>
+                    <Grid itam xs={3}>
+                        <Typography variant="h6" component="div" color="#6AB7D6">
+                        TAKAB LOOK TAKEB
+                        </Typography>
+                        <Typography>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarBorderIcon color="primary"/>
+                        <StarBorderIcon color="primary"/>
+                            </Typography>
+                        <Typography variant="body2" color="#6AB7D6">
+                        Best site Ever
+                        </Typography>
+                    </Grid>
+                </Grid>
+                </div>
+
+                <div>
+                <Grid className="Tao" container spacing={2} columns={4}>
+                    <Grid item xs={1}>
+                    <Avatar className="Tree" alt=" " src={require("./image/avatar.png")} />
+                    </Grid>
+                    <Grid itam xs={3}>
+                        <Typography variant="h6" component="div" color="#6AB7D6">
+                        JAISUE TAE MAISUE
+                        </Typography>
+                        <Typography>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                        <StarIcon color="primary"/>
+                            </Typography>
+                        <Typography variant="body2" color="#6AB7D6">
+                        SHE SAID YESSS THANK YOU GUYS
+                        </Typography>
+                    </Grid>
+                </Grid>
+                </div>
+            </div>
+            <div className="box-review">            
+                 <Typography className="Teen">
                 <FormControl> 
                     <div className="form-star" >
-                    <form className="form"   onSubmit={handleSubmit}>
-                    {/* <label for="fname"></label> */}
-                    <div className='box-ii'> 
-                    <Avatar className="avt2" alt=" " src={require("./image/avatar.png")} sx={{ width: 40, height: 40 }}/>
-                    <div className='box-iii'>  
-                    <Rating name="customized-10"   defaultValue={comment.Rating} max={5} size="large" onChange={(e) => setComment({ ...comment, Rating: e.target.value })} />
+                    <form className="form">
+                     {/* <label for="fname"></label> */}
+                     <div className='box-ii'> 
+                     <Avatar className="avt2" alt=" " src={require("./image/avatar.png")} sx={{ width: 40, height: 40 }}/>
+                     <div className='box-iii'>  
+                    <Rating name="customized-10"  max={5} size="large"  />
                     </div> 
                     </div>
                     
                                                               
-                    <input type="text1" id="fname" name="fname" className="fname" placeholder="Headers" value={comment.Headers} onChange={(e) => setComment({ ...comment, Headers: e.target.value })} />
+                    <input type="text1" id="fname" name="fname" className="fname" placeholder="Headers"  />
+                  
+                    <input type="text2" id="fname1" name="fname1" className="fname1" placeholder="Comment"  />
                     
-                    <input type="text2" id="fname1" name="fname1" className="fname1" placeholder="Comment" value={comment.Descriptions} onChange={(e) => setComment({ ...comment, Descriptions: e.target.value })} />
-                    
-                    <input type="submit" value="Send" ></input>
+                    <input type="submit" value="Send" ></input>                     
                     </form>
                     </div>
                 </FormControl> 
-                </Typography> 
+                </Typography>              
             </div>   
-            ):( <></> )}
-            
-            {/* </AppBar> */}
         </div>
         </>
     )
 }
 
-export default Review;
+export default Review; 
