252d7ec912625413bc6b2e6092daa7ffc8e6f258
Nutnarin Jowantha <6410110144@psu.ac.th>
2022-03-28 17:18:52 +0700

    package 2999 + 3999

--- liperetro/src/programtour/Package2999.js
+++ liperetro/src/programtour/Package2999.js
@@ -1,115 +1,144 @@
-// import React, { useState } from 'react';
-// import { Link } from "react-router-dom";
-// import LRbar from "../bar/Bar";
-// import './Package.css';
+import React, { useState , useEffect } from 'react';
+import { Link } from "react-router-dom";
+import LRbar from "../bar/Bar";
+import './Package.css';
+import food from './food';
+import hotel2 from './hotel2';
+import { FormControl } from 'react-bootstrap';
 
-// function Package2999() {
+function Package2999() {
 
-//     const [value, setValue] = useState('Bundhaya Resort');
+    const [value, setValue] = useState('Bann Peangpor Hostel');
 
-//     const handleChange = (event) => {
-//         setValue(event.target.value);
-//     };
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
 
-//     return (
-//         <>
-//             <LRbar />
-//             <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet" />
-//             <div className='backgrand-status2'>
-//                 <br />
-//                 <div className="pics">
-//                     <img src='https://www.chillpainai.com/src/wewakeup/img_hotels/3211/1487070806-_MG_9750.JPG' />
-//                 </div>
+    const [foodState, setFoodState] = useState();
 
-//                 <p className='description'>
-//                     In this 1999฿ Package is including with 3 island tour and a free 1 meal at your place
-//                     feel free to choose what you like with to following Hotel/Motel and Meal.
-//                 </p>
+    const onChangeComboBox = (e) => {
+        const selectedId = e.target.value;
+        const selectedFoodState = food.filter((d) => d.id == selectedId)[0];
+        setFoodState(selectedFoodState);
+    };
 
-//                 <form className='hotel-form' >
+    useEffect(() => {
+        setFoodState(food[1]);
+    }, []);
 
-//                     <h2>Hotel</h2>
-//                     {/* <div className="container"> */}
-//                     <select onChange={(e) => { setBook({ ...book, hotels: e.target.value }) }}
-//                         className="custom-select"
-//                         value={hotel?.id}
+    const [hotelState, setHotelState] = useState();
 
-//                     >
-//                         {hotel.map((item) => (
-//                             <option key={item.id} value={item.id} >
-//                                 {item.Name}
-//                             </option>
+    const onChangeComboBox1= (e) => {
+        const selectedId = e.target.value;
+        const selectedHoteltate = hotel2.filter((d) => d.id == selectedId)[0];
+        setHotelState(selectedHoteltate);
+    };
 
-//                         ))}
-//                     </select>
-//                     {hotel.id ? (
-//                         <img
-//                             src={`http://localhost:1337${hotel.Image.url}`}
-//                             width="100%"
-//                             height="100%" />
-//                     ) : (""
-//                     )}
-//                     {/* {hotel.id ? (
-//                             <img
-//                             src={`http://localhost:1337${hotel.Image.url}`}
-//                             width="100%"
-//                             height="100%"/>
-//                         ) : ( ""
-//                             )} */}
-//                     {/* </div> */}
+    useEffect(() => {
+        setHotelState(hotel2[1]);
+    }, []);
 
-//                     {/* </form> */}
+    return (
+        <>
+            <LRbar />
+            <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet" />
+            <div className='backgrand-status2'>
+                <br />
+                <div className="pics">
+                    <img src='https://www.chillpainai.com/src/wewakeup/scoop/images/2653e8c3228b8245308f00ad1cffe403c0ce5ea1.jpg' />
+                </div><br/>
+                <h2 className='packname'>Package 2999</h2>
+                <p className='description'>
+                In this 2999฿ Package is including with 6 island tours, 3 outdoor activities and free 2 meals at your place feel free to choose what you like with to following Hotel/Motel and Meal.
+                </p>
 
-//                     <br /><br />
+                    <form className='hotel-form'>
 
-//                     {/* <form className='meal-form'> */}
+                        <h2>Hotel</h2>
+                        <div className="container">
+                            <select
+                                className="custom-select"
+                                value={hotelState?.id}
+                                onChange={(e) => {
+                                    onChangeComboBox1(e);
+                                }}
+                            >
+                                {hotel2.map((d) => (
+                                    <option key={d.id} value={d.id}>
+                                        {d.hotel}
+                                    </option>
+                                ))}
+                            </select><br/><br/>
+                            {hotelState ? (
+                                <img src={hotelState?.image} width="100%" height="500px" />
+                            ) : (
+                                ""
+                            )}
+                        </div>
 
-//                     <h2>Meal</h2>
-//                     {/* <div className="container"> */}
-//                     <select onChange={(e) => setBook({ ...book, foods: e.target.value })}
-//                         className="custom-select"
-//                         value={food?.id}
+                    </form>
 
-//                     >
-//                         {food.map((item) => (
-//                             <option key={item.id} value={item.id}>
-//                                 {item.Coursename}
-//                             </option>
-//                         ))}
-//                     </select>
-//                     <h1 className='topic'>
-//                         Date
-//                         <FormControl value={book.Date} onChange={(e) => setBook({ ...book, Date: e.target.value })} type="date" required></FormControl>
-//                     </h1>
+                    <br />
 
+                    <form className='meal-form'>
 
-//                     <h1 className='topic'>
-//                         Additional :
-//                     </h1>
-
-
-//                     <input type="text" className='additional' rows="2" value={book.Additional} onChange={(e) => setBook({ ...book, Additional: e.target.value })} required />
-
-//                     <h1 className='topic'>
-//                         Person
-//                     </h1>
-
-//                     <input min="1" max="10" className='person' type="number" value={book.Person} onChange={(e) => setBook({ ...book, Person: e.target.value })} />
-//                     <div className='price-booking' value={book.Prices = pack.Price * book.Person} onChange={(e) => setBook({ ...book, Prices: e.target.value })}>Total : {pack.Price * book.Person} Baht</div>
-//                     {/* <a value={pack.Price*book.Person} onChange={(e) => setBook({ ...book, Prices: e.target.value })}>{pack.Price*book.Person}</a> */}
-
-//                 </form>
-
-//                 <Link state={{ book: book }} to="/payment" className="btn-booking">Booking</Link>
-//                 <a href='/package' className='btn-back'>Back</a>
+                        <h2>Meal</h2>
+                        <div className="container">
+                            <select
+                                className="custom-select"
+                                value={foodState?.id}
+                                onChange={(e) => {
+                                    onChangeComboBox(e);
+                                }}
+                            >
+                                {food.map((d) => (
+                                    <option key={d.id} value={d.id}>
+                                        {d.restaurant}
+                                    </option>
+                                ))}
+                            </select><br/><br/>
+                            {foodState ? (
+                                <img src={foodState?.image} />
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                    </form>
+                    
+                    <div className='info-form'>
+                    <h1 className='topic'>
+                        Date
+                        <br/>
+                        <FormControl type="date" required style={{color:'rgb(68, 68, 68)'}}  class="form-control-lg" fontFamily='Mitr'></FormControl>
+                    </h1>
 
 
-//                 <br />
+                    <h1 className='topic'>
+                        Additional :
+                    </h1>
 
 
-//             </div>
-//         </>
-//     )
-// }
+                    <input type="text" className='additional' rows="2" required />
 
-// export default Package2999;
+                    <h1 className='topic'>
+                        Person
+                    </h1>
+                    
+                    <input min="1" max="10" className='person' type="number" required/>
+                    <div className='price-booking' >Total : *** Baht</div>
+                    {/* <a ></a> */}
+                    </div>
+
+                <Link to="/payment" className="btn-booking">Booking</Link>
+                <a href='/package' className='btn-back'>Back</a>
+
+
+                <br />
+
+
+            </div>
+        </>
+    )
+}
+
+export default Package2999;
